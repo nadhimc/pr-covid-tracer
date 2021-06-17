051cff05bc5a4af8b7511c3a3d395f7d8ed62359
nadhimc <chafidznadhim@gmail.com>
2021-06-17 18:15:43 +0700

    test deploy

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -7,7 +7,8 @@ class Isitabel extends React.Component{
         super(props);
         this.state = {
             isLoaded : false,
-            persons : []
+            persons : [],
+            isError : false,
         }
     }
 
@@ -18,60 +19,77 @@ class Isitabel extends React.Component{
             (result)=>{
                 this.setState({
                     isLoaded: true,
-                    persons : result
+                    persons : result,
                 })
             },
             (error)=>{
                 console.log(error);
+                this.setState({
+                    isError: true,
+                    isLoaded: true
+                })
             }
         )
     }
 
     render(){
-        const {persons,isLoaded} = this.state;
+        const {persons,isLoaded,isError} = this.state;
 
         if(isLoaded){
             // sudah Loading
-            return(
-                persons.filter((val)=>{
-                    if(this.props.search === ""){
-                        return val
-                    }else if(val.name.toLowerCase().includes(this.props.search.toLowerCase())){
-                        return val
-                    }
-                }).map((item,index)=>
-                    <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
-                        <td className="py-3 px-6 text-center">{index+1} {this.state.test}</td>
-                        <td className="py-3 px-6 text-center">{item.person_id}</td>
-                        <td className="py-3 px-6 text-center">{item.name}</td>
-                        <td className="py-3 px-6 text-center">{item.gender}</td>
-                        <td className="py-3 px-6 text-center">{item.age}</td>
-                        <td className="py-3 px-6 text-center">{item.conditition}</td>
-                        <td className="py-3 px-6 text-center">
-                            <div className="flex item-center justify-center">
-                                <Link to={"tracker/"+item.person_id}>
-                                    <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                        </svg>
-                                    </div>
-                                </Link>
-                                {/* <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </div>
-                                <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                </div> */}
-                            </div>
+            if(isError){
+                return(
+                    <tr>
+                        <td colSpan="7" className="text-center">
+
+                            {/* <img alt="loading..." className="mx-auto" src={LoadingImage} style={{maxWidth: '200px'}} /> */}
+                            <p className="text-center mx-auto text-lg font-semibold mb-3">Error</p>
+
                         </td>
                     </tr>
                 )
-            )
+            }else{
+                return(
+                    persons.filter((val)=>{
+                        if(this.props.search === ""){
+                            return val
+                        }else if(val.name.toLowerCase().includes(this.props.search.toLowerCase())){
+                            return val
+                        }
+                    }).map((item,index)=>
+                        <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
+                            <td className="py-3 px-6 text-center">{index+1} {this.state.test}</td>
+                            <td className="py-3 px-6 text-center">{item.person_id}</td>
+                            <td className="py-3 px-6 text-center">{item.name}</td>
+                            <td className="py-3 px-6 text-center">{item.gender}</td>
+                            <td className="py-3 px-6 text-center">{item.age}</td>
+                            <td className="py-3 px-6 text-center">{item.conditition}</td>
+                            <td className="py-3 px-6 text-center">
+                                <div className="flex item-center justify-center">
+                                    <Link to={"tracker/"+item.person_id}>
+                                        <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                            </svg>
+                                        </div>
+                                    </Link>
+                                    {/* <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                        </svg>
+                                    </div>
+                                    <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                    </div> */}
+                                </div>
+                            </td>
+                        </tr>
+                    )
+                )
+            }
         }else{
             // Belum Loading
             return(
